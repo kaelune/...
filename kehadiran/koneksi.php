0b1a1cae53455a096f75d891a5d5fae8ad26635f
Pilar Filino Hadi <102205608+kaelune@users.noreply.github.com>
2023-12-01 22:33:53 +0700

    Ganti port dari 3307 menjadi 3306

--- kehadiran/koneksi.php
+++ kehadiran/koneksi.php
@@ -6,5 +6,5 @@ header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Type');
 header('Content-Type: application/json, charset=utf-8');
 //membuat variable koneksi ke mysql
-$koneksi = mysqli_connect('localhost','root','','kehadiran',3307) or die ('koneksi
+$koneksi = mysqli_connect('localhost','root','','kehadiran') or die ('koneksi
 gagal');
